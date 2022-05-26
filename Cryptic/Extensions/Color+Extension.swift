4eebfaae8dc8fdd7d18a5565640a2f2ff6a4fc01
jmkim <wjdals0304@gmail.com>
2022-05-26 14:58:13 +0900

    add: color file add

---
+++ Cryptic/Extensions/Color+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extension.swift
+//  Cryptic
+//
+//  Created by 김정민 on 2022/05/26.
+//
+
+import UIKit
+
+extension UIColor {
+    static var appBackground: UIColor {
+        return UIColor(red: 245.0 / 255.0, green: 245.0 / 255.0, blue: 245.0 / 255.0, alpha: 1)
+    }
+}
