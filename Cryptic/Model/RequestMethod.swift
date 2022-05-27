9a2646c73feb075e2b6077ea457e5111c20f5368
jmkim <wjdals0304@gmail.com>
2022-05-27 11:55:55 +0900

    add: requestmedthod file add

---
+++ Cryptic/Model/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  Cryptic
+//
+//  Created by 김정민 on 2022/05/26.
+//
+
+import Foundation
+
+enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
