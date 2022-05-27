16d6006925cc7000bc4587b31af9d76ea4ed0838
jmkim <wjdals0304@gmail.com>
2022-05-27 17:19:35 +0900

    mod: init() method add

--- Cryptic/UIKit/ViewController/BaseVC.swift
+++ Cryptic/UIKit/ViewController/BaseVC.swift
@@ -23,6 +23,13 @@ class BaseVC: UIViewController {
         setupConstraint()
         bind()
     }
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     func setup() {
         
