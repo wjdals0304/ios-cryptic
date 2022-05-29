902dfb59c91254871065aca97649892b44fad53c
jmkim <wjdals0304@gmail.com>
2022-05-29 12:35:10 +0900

    add: error case 추가

--- Cryptic/Model/AppError.swift
+++ Cryptic/Model/AppError.swift
@@ -11,7 +11,10 @@ enum AppError: Error, Equatable {
     case badURL(description: String)
     case parsing(description: String)
     case network(description: String)
-
+    case noResponse(description: String)
+    case unauthorized(description: String)
+    case unexpectedStatusCode(description: String)
+    
     var description: String {
         switch self {
         case .badURL(let value):
@@ -20,6 +23,12 @@ enum AppError: Error, Equatable {
             return value
         case .network(let value):
             return value
+        case .noResponse(let value):
+            return value
+        case .unauthorized(let value):
+            return value
+        case .unexpectedStatusCode(let value):
+            return value 
         }
     }
 }
