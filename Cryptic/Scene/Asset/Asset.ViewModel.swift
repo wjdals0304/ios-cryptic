5ea9b43a51e179fc9c1333c2307427cab47932cd
jmkim <wjdals0304@gmail.com>
2022-05-29 12:33:24 +0900

    add: viewModel 구현

--- Cryptic/Scene/Asset/Asset.ViewModel.swift
+++ Cryptic/Scene/Asset/Asset.ViewModel.swift
@@ -6,9 +6,35 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol AssetViewModelType {
+    func fetchAssets() async throws
+    var assetListGet: AssetList { get }
+}
 
 extension Asset {
+    
     final class ViewModel {
+        @Injected private var assetService: AssetService
+        var assetList: AssetList = AssetList(data: [])
         
     }
+}
+
+extension Asset.ViewModel: AssetViewModelType {
+    
+    var assetListGet: AssetList {
+        return assetList
+    }
+    
+    func fetchAssets() async throws {
+        do {
+            let result = try await assetService.fetchAssets()
+            self.assetList = result
+        } catch {
+            throw error
+        }        
+    }
+    
 }
